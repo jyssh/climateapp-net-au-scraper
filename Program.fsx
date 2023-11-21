1db3dfb701da1d509847be1925567f4e83176ccc
Jayesh Bhoot <jysh@jysh.net>
2023-11-21 15:34:05 +0530

    Implement loading and scraping of a submitted analysis page

--- Program.fsx
+++ Program.fsx
@@ -55,10 +55,39 @@ let getUser (tabTask: Task<IPage>) =
         return! ele.TextContentAsync()
     }
 
+let loadHowsTheSeasonPage (tabTask: Task<IPage>) =
+    task {
+        let! tab = tabTask
+        let! _ = tab.GotoAsync("https://climateapp.net.au/A03_HowsTheSeason")
+        let waitingLocator = tab.GetByText("Preparing analysis")
+        // let! () = Assertions.Expect(waitingLocator).Not.ToBeVisibleAsync()
+        let! _ = tab.WaitForSelectorAsync(".analysis-content-ready")
+        let! () = tab.WaitForTimeoutAsync(50000f)
+        return tab
+    }
+    
+let screenshotPage (tabTask: Task<IPage>) =
+    task {
+        let! tab = tabTask
+        let opts = PageScreenshotOptions(FullPage=true, Path="screenshot.jpg")
+        let! _ = tab.ScreenshotAsync(opts)
+        return tab
+    }
+    
+let savePage (tabTask: Task<IPage>) =
+    task {
+        let! tab = tabTask
+        let! content = tab.ContentAsync()
+        IO.File.WriteAllText("page.html", content)
+        return tab
+    }
+
 makeBrowserContext ()
 |> setAuthCookie
-|> makeTab (url)
-|> getUser
+|> makeTab url
+|> loadHowsTheSeasonPage
+|> screenshotPage
+|> savePage
 |> Async.AwaitTask
 |> Async.RunSynchronously
 |> Console.WriteLine
